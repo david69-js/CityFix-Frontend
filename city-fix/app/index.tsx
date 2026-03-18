d44371fb4727b0788e63f061e6e34009ff734bf5
David <da3909622@gmail.com>
2026-03-17 23:48:09 -0600

    Reset project

---
+++ city-fix/app/index.tsx
@@ -0,0 +1,16 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Todos los cambios desde mi mac</Text>
+      <Text>Come bicho jajajaj</Text>
+    </View>
+  );
+}
